a015d1d1339d5f175ff6e942f13111d84b8f43a1
ilkinulas <ilkinulas@gmail.com>
2012-08-18 15:49:47 +0300

    documentation

--- game.lua
+++ game.lua
@@ -1,37 +1,19 @@
 require('backgammon')
 
+--[[
+Backgammon game simulation.
+]]
+
 player1 = 1
 player2 = 2
 board = createBoard()
 
-function score1(player, previousBoard, board)	
-	local pointsCoefficient = 0.5
-	local bearingOffCoefficient = 0.4
-	local myBlotsCoefficient = 0.3
-	local opponentCoefficient = 0.2
-
-	local diffBearingOfCheckers = numberOfBearingOffCheckers(player, board) - numberOfBearingOffCheckers(player, previousBoard)
-	local diffMyNumberOfBlots = numberOfBlots(player, board) - numberOfBlots(player, previousBoard)
-	local diffOpponentNumberOfBLots = numberOfBlots(opponent(player), board) - numberOfBlots(opponent(player), previousBoard)
-
-	if isCollecting(player, board) then
-		pointsCoefficient = 0.0
-		myBlotsCoefficient = 0.0
-	end
-
-	return numberOfPoints(player, board) * pointsCoefficient + diffBearingOfCheckers * bearingOffCoefficient - diffMyNumberOfBlots * myBlotsCoefficient + diffOpponentNumberOfBLots * opponentCoefficient
-end
-
-function score2(player, previousBoard, board)
-	return 0
-end
-
-local interactive = false
+local debugMode = false
 
 while true do
 	print("PLAYER 1 is playing...")
 	local dice = rollDice()
-	local move = play(player1, board, dice, score1)	
+	local move = play(player1, board, dice, selectBestMove)	
 	if move == nil then
 		print("Can not play, waiting...")
 	else 
@@ -44,12 +26,12 @@ while true do
 	end
 
 	
-	if interactive then local line = io.read() end
+	if debugMode then local line = io.read() end
 
 	print("PLAYER 2 is playing...")
 	dice = rollDice()
 
-	move = play(player2, board, dice, score2)
+	move = play(player2, board, dice, nil)
 	if move == nil then
 		print("Can not play, waiting...")
 	else 
@@ -62,5 +44,5 @@ while true do
 
 	end
 	
-	if interactive then local line = io.read() end
+	if debugMode then local line = io.read() end
 end
